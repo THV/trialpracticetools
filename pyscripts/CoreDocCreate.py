adef9739627b8152a57ad7ace19bf01be140cc4f
THV <thomashvidal@gmail.com>
2021-11-20 20:33:00 -0800

    Pre-release version 5

--- pyscripts/CoreDocCreate.py
+++ pyscripts/CoreDocCreate.py
@@ -6,21 +6,17 @@ from datetime import date
 
 import sys
 import codecs
-print(sys.version)
 UTF8Writer = codecs.getwriter('utf8')
 
-coredoc01_tmpl = "/Users/tvidal/Dropbox/Templates/99.Core Docs/CoreDoc01_finder_template.docx"
-coredoc02_tmpl = "/Users/tvidal/Dropbox/Templates/99.Core Docs/CoreDoc02_litplan_template.docx"
-coredoc10_tmpl = "/Users/tvidal/Dropbox/Templates/99.Core Docs/CoreDoc10_case_questions_template.docx"
-
+#Associate the Core Document Templates
+coredoc01_tmpl = "/Users/tvidal/Documents/Templates/99.Core Docs/CoreDoc01_finder_template.docx"
+coredoc02_tmpl = "/Users/tvidal/Documents/Templates/99.Core Docs/CoreDoc02_litplan_template.docx"
+coredoc10_tmpl = "/Users/tvidal/Documents/Templates/99.Core Docs/CoreDoc10_case_questions_template.docx"
 
 #Assign the templates to the core merge documents
 coredoc01 = MailMerge(coredoc01_tmpl)
 coredoc02 = MailMerge(coredoc02_tmpl)
 coredoc10 = MailMerge(coredoc10_tmpl)
-
-
-print(coredoc01.get_merge_fields())
 
 #Assign kwargs
 kwargs = ({
@@ -30,13 +26,12 @@ kwargs = ({
     "ct_file_no": 'GCC-123'
     })
 
-
 #Merge the documents
 coredoc01.merge(**kwargs)
 coredoc02.merge(**kwargs)
 coredoc10.merge(**kwargs)
 
 #Write the documents
-coredoc01.write('/Users/tvidal/Desktop/Core Doc 01 - Info Sheet.docx')
-coredoc02.write('/Users/tvidal/Desktop/Core Doc 02 - Litigation Plan.docx')
-coredoc10.write('/Users/tvidal/Desktop/Core Doc 10 - Questions & Probs.docx')
+coredoc01.write('/Users/tvidal/Desktop/Completed Templates/Core Doc 01 - Info Sheet.docx')
+coredoc02.write('/Users/tvidal/Desktop/Completed Templates/Core Doc 02 - Litigation Plan.docx')
+coredoc10.write('/Users/tvidal/Desktop/Completed Templates/Core Doc 10 - Questions & Probs.docx')
